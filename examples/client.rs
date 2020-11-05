4755166c832362a4b3f14486ae5042e10692651d
syariefa <syareeefa@gmail.com>
2020-11-05 15:30:41 +0700

    split read dan write tcp client

--- examples/client.rs
+++ examples/client.rs
@@ -1,12 +1,24 @@
-use std::error::Error;
 use tokio::net::TcpStream;
-use tokio::prelude::io::AsyncWriteExt;
+use tokio::io::{self, AsyncWriteExt, AsyncReadExt};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let mut client = TcpStream::connect("127.0.0.1:8080").await?;
+async fn main() -> io::Result<()> {
+    let client = TcpStream::connect("127.0.0.1:8080").await?;
+    let (mut rd, mut wr) = io::split(client);
+    let mut buf = vec![0; 1024];
 
-    client.write_all(b"Ini baru nyoba aja...").await?;
+    tokio::task::spawn(async move {
+        wr.write_all(b"percobaan 1\n").await.unwrap();
+        wr.write_all(b"percobaan 2").await.unwrap();
+    });
+
+    loop {
+        let len = rd.read(&mut buf).await?;
+        let message = String::from_utf8_lossy(&buf[..len]);
+
+        if len == 0 { break; }
+        println!("Client got: {:?}", message);
+    }
 
     Ok(())
 }
